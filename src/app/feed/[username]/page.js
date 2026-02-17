bafdc178a9b84b6403c01b0979bc72a51687485f
Sinéad McIntyre <shins.mcintyre@gmail.com>
2026-02-17 10:36:41 +0000

    feat: set up for matches page and component

--- src/app/feed/[username]/page.js
+++ src/app/feed/[username]/page.js
@@ -1,3 +1,40 @@
+import Header from "@/components/Header";
+import NavBar from "@/components/NavBar";
+import MatchesList from "@/components/MatchesList"
+import feedStyles from "@/app/feed/[username]/feed.module.css"
+import {FaSort, FaFilter, FaGripLinesVertical} from "react-icons/fa"
+
 export default function feedPage() {
-  return <h1>Feed Page</h1>;
+  return (
+    <>
+    <Header/>
+
+    <NavBar/>
+
+    <h1>Your matches</h1>
+
+    <hr></hr>
+
+    <button>
+      <FaSort className={feedStyles.icon}/>
+      <span className={feedStyles.label}>Sort</span>
+    </button>
+
+    <FaGripLinesVertical className={feedStyles.lines}/>
+
+    <button>
+      <FaFilter className={feedStyles.icon}/>
+      <span className={feedStyles.label}>Filter</span>
+    </button>
+
+    <hr></hr>
+
+    <MatchesList/>
+
+    <Footer/>
+    
+    </>
+  )
 }
+
+
--- src/app/profile/[username]/profile-page/page.js
+++ src/app/profile/[username]/profile-page/page.js
@@ -1,5 +1,6 @@
 import Header from "src/components/Header";
 import NavBar from "@/components/NavBar";
+import profilePageStyles from "@/app/profile/[username]/profile-page/profile-page.module.css"
 
 export default function profilePage() {
   return(
