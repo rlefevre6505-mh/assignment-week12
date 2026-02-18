e8f1ad26e06838fcc74b7626448e544d8ed6316b
AMY LOUISE TUCK <amyltuck@hotmail.com>
2026-02-18 16:51:17 +0000

    style: add styling to sign up and sign in pages to match user flow

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -2,27 +2,40 @@
 import * as SignIn from "@clerk/elements/sign-in";
 import * as Clerk from "@clerk/elements/common";
 // import { ClerkLoaded } from "@clerk/nextjs";
+import signinStyles from "./sign-in.module.css";
 
 export default function signInPage() {
   return (
-    <div>
-      {/* <ClerkLoaded> */}
-      <SignIn.Root>
-        <SignIn.Step name="start">
-          <Clerk.Field name="identifier">
-            <Clerk.Label>Email</Clerk.Label>
-            <Clerk.Input placeholder="enter your email address" />
-            <Clerk.FieldError className="field-error" />
-          </Clerk.Field>
-          <Clerk.Field name="password">
-            <Clerk.Label>Password</Clerk.Label>
-            <Clerk.Input placeholder="enter your password" />
-            <Clerk.FieldError className="field-error" />
-          </Clerk.Field>
-          <SignIn.Action submit>Sign In</SignIn.Action>
-        </SignIn.Step>
-      </SignIn.Root>
-      {/* </ClerkLoaded> */}
-    </div>
+    <main className={signinStyles.main}>
+      <h1 className={signinStyles.heading}>Sign in here</h1>
+      <h2 className={signinStyles.subheading}>Sign in to see your matches</h2>
+      <section className={signinStyles.form}>
+        {/* <ClerkLoaded> */}
+        <SignIn.Root>
+          <SignIn.Step name="start">
+            <Clerk.Field name="identifier" className={signinStyles.field}>
+              <Clerk.Label>Email:</Clerk.Label>
+              <Clerk.Input
+                placeholder="Enter your email address"
+                className={signinStyles.input}
+              />
+              <Clerk.FieldError className="field-error" />
+            </Clerk.Field>
+            <Clerk.Field name="password" className={signinStyles.field}>
+              <Clerk.Label>Password:</Clerk.Label>
+              <Clerk.Input
+                placeholder="Enter your password"
+                className={signinStyles.input}
+              />
+              <Clerk.FieldError className="field-error" />
+            </Clerk.Field>
+            <SignIn.Action submit className={signinStyles.button}>
+              Sign In
+            </SignIn.Action>
+          </SignIn.Step>
+        </SignIn.Root>
+        {/* </ClerkLoaded> */}
+      </section>
+    </main>
   );
 }
